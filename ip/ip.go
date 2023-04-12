a21a62ff4d627d6f47c5a612d01723426d5bfb3c
lnzx <lnx@tutamail.com>
2023-04-12 17:33:32 +0800

    bogon ip

--- ip/ip.go
+++ ip/ip.go
@@ -1,12 +1,40 @@
 package ip
 
 import (
-	"crypto/rand"
+	randc "crypto/rand"
+	"math/rand"
 	"net"
 )
 
-func IPv4() string {
+type ipOptions struct {
+	bogon bool
+}
+
+type Option interface {
+	apply(o *ipOptions)
+}
+
+type funcOption func(*ipOptions)
+
+func (f funcOption) apply(o *ipOptions) {
+	f(o)
+}
+
+func WithBogon() Option {
+	return funcOption(func(o *ipOptions) {
+		o.bogon = true
+	})
+}
+
+func IPv4(options ...Option) string {
+	opts := ipOptions{}
+	for _, opt := range options {
+		opt.apply(&opts)
+	}
 	ip := make(net.IP, net.IPv4len)
-	_, _ = rand.Read(ip)
+	_, _ = randc.Read(ip)
+	if !opts.bogon {
+		ip[0] = byte(1 + rand.Intn(223))
+	}
 	return ip.String()
 }
